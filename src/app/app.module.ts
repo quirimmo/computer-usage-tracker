11b19917d51dbe5e54bf4515965df092ad713e3b
Quirino Lo Russo <quirimmo@gmail.com>
2018-10-23 00:25:34 +0200

    added redux and scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgReduxModule } from '@angular-redux/store';
+import { NgReduxRouterModule } from '@angular-redux/router';
 import { AppComponent } from './app.component';
-import { StoreModule } from './store/store.module';
+import { StoreModule } from './store/module';
+import { RouterModule } from '@angular/router';
+import { AppRoutes } from './app.routes';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, NgReduxModule, StoreModule],
+	imports: [
+		RouterModule.forRoot(AppRoutes),
+		BrowserModule,
+		NgReduxModule,
+		NgReduxRouterModule.forRoot(),
+		StoreModule
+	],
 	providers: [],
 	bootstrap: [AppComponent]
 })
---
+++ src/app/store/module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import {
+	DevToolsExtension,
+	NgRedux,
+	NgReduxModule
+} from '@angular-redux/store';
+import { provideReduxForms } from '@angular-redux/form';
+import { NgReduxRouter, NgReduxRouterModule } from '@angular-redux/router';
+import { StoreModel } from './model';
+import { StoreReducer } from './reducers';
+import thunk from 'redux-thunk';
+
+@NgModule({
+	imports: [NgReduxModule, NgReduxRouterModule.forRoot()],
+	providers: []
+})
+export class StoreModule {
+	constructor(
+		public store: NgRedux<StoreModel>,
+		devTools: DevToolsExtension,
+		ngReduxRouter: NgReduxRouter
+	) {
+		store.configureStore(
+			StoreReducer,
+			{},
+			[thunk],
+			devTools.isEnabled() ? [devTools.enhancer()] : []
+		);
+
+		// Enable syncing of Angular router state with our Redux store.
+		if (ngReduxRouter) {
+			ngReduxRouter.initialize();
+		}
+
+		// Enable syncing of Angular form state with our Redux store.
+		provideReduxForms(store);
+	}
+}
