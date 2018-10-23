238884621bbf19e23573774181b721be7d1754ed
Quirino Lo Russo <quirimmo@gmail.com>
2018-10-24 00:46:30 +0200

    refactored code

--- src/app/users/actions.ts
+++ src/app/users/actions.ts
@@ -1,7 +1,7 @@
 import { dispatch } from '@angular-redux/store';
 import { Injectable } from '@angular/core';
 import { User } from './model';
-import { AnyAction, Action } from 'redux';
+import { Action } from 'redux';
 
 export interface UserAction extends Action {
 	users?: User[];
@@ -18,32 +18,4 @@ export class UserActions {
 			users: [new User('Quirino'), new User('Pelota')]
 		};
 	}
-
-	// fetch = (animalType: AnimalType): Action => ({
-	// 	type: AnimalAPIActions.LOAD_ANIMALS,
-	// 	meta: { animalType },
-	// 	payload: null
-	// });
-
-	// loadStarted = (animalType: AnimalType): AnimalAPIAction => ({
-	// 	type: AnimalAPIActions.LOAD_STARTED,
-	// 	meta: { animalType },
-	// 	payload: null
-	// });
-
-	// loadSucceeded = (
-	// 	animalType: AnimalType,
-	// 	payload: Payload
-	// ): AnimalAPIAction => ({
-	// 	type: AnimalAPIActions.LOAD_SUCCEEDED,
-	// 	meta: { animalType },
-	// 	payload
-	// });
-
-	// loadFailed = (animalType: AnimalType, error): AnimalAPIAction => ({
-	// 	type: AnimalAPIActions.LOAD_FAILED,
-	// 	meta: { animalType },
-	// 	payload: null,
-	// 	error
-	// });
 }
